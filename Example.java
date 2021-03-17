56e6ddbfa936cb16cd76bfa572a6ebe1f1feda99
Jayabal Manoharan <mjayabalpsg@gmail.com>
2021-03-17 08:42:07 +0530

    Create Example.java

---
+++ Example.java
@@ -0,0 +1,18 @@
+
+// Online IDE - Code Editor, Compiler, Interpreter
+
+public class Main
+{
+    public static void main(String[] args) {
+        //System.out.println(Short.MAX_VALUE);
+        //System.out.println(Short.MIN_VALUE);
+        int t = Short.MAX_VALUE & 0xffff;
+        short s = -536; 
+        int f = s;
+        System.out.println(Integer.toBinaryString(536));
+        System.out.println(Integer.toBinaryString(f));
+        //System.out.println(f);
+        //System.out.println(s & 0xffff);    
+    
+    }
+}
